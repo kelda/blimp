6ce9edce4c8ea4da6160da4da580208352b263da
Kevin Lin <kevin@kelda.io>
2020-03-19 20:36:35 -0700

    scripts: Add script for creating clusters

---
+++ scripts/create-prod.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# This script should be invoked from the root of the Kelda Blimp repo.
+
+function _gcloud_kube() {
+    gcloud container clusters --project "${PROJECT}" --zone us-west1-a $@
+}
+
+PROJECT="kelda-blimp"
+
+_gcloud_kube create customer -m e2-standard-2 --num-nodes=2 --no-enable-autoupgrade
+_gcloud_kube get-credentials customer
+./scripts/make-kubeconfig.sh > /tmp/customer-cluster-kubeconfig
+
+_gcloud_kube create manager -m e2-standard-2 --num-nodes=1 --no-enable-autoupgrade
+_gcloud_kube get-credentials manager
+kubectl create secret generic customer-cluster-kubeconfig --from-file=config=/tmp/customer-cluster-kubeconfig
+kubectl apply -f ./cluster-controller/kube
+
+echo "Deployed the customer and manager clusters. Run 'kubectl get services' to get the manager's public IP."
