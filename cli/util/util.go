ef0e88b9acd6c2682baadf54216461dfea5eaf59
Ethan J. Jackson <ethan@kelda.io>
2020-03-30 18:28:00 -0700

    manager: Change default port to 443

--- cli/util/util.go
+++ cli/util/util.go
@@ -3,22 +3,11 @@ package util
 import (
 	"crypto/x509"
 	"errors"
-	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/encoding/gzip"
 )
-
-var ManagerHost = getManagerHost()
-
-func getManagerHost() string {
-	envVal := os.Getenv("MANAGER_HOST")
-	if envVal != "" {
-		return envVal
-	}
-	return "blimp-manager.kelda.io:9000"
-}
 
 func Dial(addr, certPEM string) (*grpc.ClientConn, error) {
 	cp := x509.NewCertPool()
