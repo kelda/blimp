209ed2c1af680064ac3806fd3e257cdd3dc9cf74
Kevin Lin <kevin@kelda.io>
2020-05-11 20:31:16 -0700

    Remove examples directory
    It's stale since we have a separate repo for the node todo application,
    and an upcoming commit will add Compose files for CI.

--- examples/node-todo/app/models/todo.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = mongoose.model('Todo', {
-    text: {
-        type: String,
-        default: ''
-    }
-});
--- examples/node-todo/public/js/core.js
+++
@@ -1,1 +0,0 @@
-angular.module('scotchTodo', ['todoController', 'todoService']);
