a34676515c2618ce49cd15d346a454a7801177e5
Kevin Lin <kevin@kelda.io>
2020-06-06 12:53:37 -0700

    scripts: Don't hardcode node names in ulimits script

--- scripts/set-docker-ulimits.sh
+++ scripts/set-docker-ulimits.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -e
 
-nodes=(
-	gke-customer-pool-1-68ed6d5a-j6gm
-	gke-customer-pool-1-68ed6d5a-kv4j
-	gke-customer-pool-1-68ed6d5a-mldm
-	gke-customer-pool-1-68ed6d5a-mxg1
-	gke-customer-pool-1-68ed6d5a-nn3l
-	gke-customer-pool-1-68ed6d5a-p4p2
-	gke-customer-pool-1-68ed6d5a-s4lk
-	gke-customer-pool-1-68ed6d5a-v9jp
-)
+PROJECT="kelda-blimp"
+ZONE="us-west1-a"
+CLUSTER_NAME="customer"
+nodes=$(gcloud compute instances list \
+    --project="${PROJECT}" \
+    --filter="metadata.items.key['cluster-name']['value']=${CLUSTER_NAME}" \
+    --format="value(name)")
+
+echo "Going to update the following nodes:"
+echo "${nodes}"
+echo
+read -p "Hit enter to continue, or Ctrl-C to exit"
 
 for node in ${nodes[@]}; do
 	echo $node
-	gcloud compute --project kelda-blimp ssh "$node" --zone us-west1-a --command "echo '{\"default-ulimits\":{\"memlock\":{\"Name\":\"memlock\",\"Hard\":-1,\"Soft\":-1}}}' | sudo tee /etc/docker/daemon.json && sudo systemctl restart docker"
+	gcloud compute --project "${PROJECT}" ssh "$node" --zone "${ZONE}" --command "echo '{\"default-ulimits\":{\"memlock\":{\"Name\":\"memlock\",\"Hard\":-1,\"Soft\":-1}}}' | sudo tee /etc/docker/daemon.json && sudo systemctl restart docker"
 done
