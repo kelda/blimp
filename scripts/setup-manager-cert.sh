fbd9de8660a78df12361369fe0c0ceb717d7835b
Kevin Lin <kevin@kelda.io>
2020-06-24 11:12:49 -0700

    scripts: Add manager cert to repo
    Before, we injected it with an environment variable in Circle, but that
    made it so the build failed on PRs. The CLI doesn't need the certificate
    key anyways, so there's no reason to hide it.

--- scripts/setup-manager-cert.sh
+++ scripts/setup-manager-cert.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 set -e
 
+# Must be invoked from the root of the repo.
+
 desired_path="$1"
 
 mkdir -p certs
 
-# Write the cert in the environment variable if it exists.
-if [[ -n "${MANAGER_CERT_BASE64}" ]] ; then
-	base64 -d <<< "${MANAGER_CERT_BASE64}" > "${desired_path}"
-	exit 0
-fi
-
-# Otherwise, fallback to loading the cert from the manager repo.
+# Write the cert from the local manager repo if it exists.
 local_cert_path="${GOPATH}/src/github.com/kelda-inc/blimp/certs/cluster-manager.crt.pem"
 if [[ -f "${local_cert_path}" ]]; then
 	ln -s "${local_cert_path}" "${desired_path}"
 	exit 0
 fi
 
-echo "Cert not found. Aborting."
-exit 1
+# Otherwise, fallback to using the production cert.
+ln -s ../scripts/cluster-manager.crt.pem "${desired_path}"
