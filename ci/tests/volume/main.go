e325c8d77308d2af0df71cb2bef8d27f2d8e2dd1
Kevin Lin <kevin@kelda.io>
2020-06-27 08:42:23 -0700

    ci: Add test for volume persistence

--- ci/tests/volume/main.go
+++ ci/tests/volume/main.go
@@ -10,6 +10,7 @@ type Test struct{}
 func (test Test) Run(ctx context.Context, t *testing.T) {
 	t.Run("Shared", func(t *testing.T) { testSharedVolumes(ctx, t) })
 	t.Run("Permissions", func(t *testing.T) { testVolumePermissions(ctx, t) })
+	t.Run("Persistence", func(t *testing.T) { testPersistence(ctx, t) })
 }
 
 func (test Test) GetName() string {
