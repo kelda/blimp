f00ca2c418231a5d5506ca1820f586ae8c2b8047
Christopher Cooper <christopher@kelda.io>
2020-07-06 13:14:50 -0700

    cluster: Handle dots in node names
    Pod names cannot include dots, but node names on EKS are FQDNs. To handle this,
    we just convert `.` to `-` in the pod name.

--- cluster-controller/node/names.go
+++ cluster-controller/node/names.go
@@ -2,10 +2,14 @@ package node
 
 import (
 	"fmt"
+	"strings"
 )
 
 func nodeControllerName(node string) string {
-	return fmt.Sprintf("node-controller-%s", node)
+	// Nodes on EKS may be full FQDNs including dots, but pod names cannot
+	// include dots.
+	sanitized := strings.ReplaceAll(node, ".", "-")
+	return fmt.Sprintf("node-controller-%s", sanitized)
 }
 
 func certSecretName(node string) string {
