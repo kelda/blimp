2296a2edc6e83804413c7850fb8f824be2802cbf
Christopher Cooper <christopher@kelda.io>
2020-07-16 17:12:57 -0700

    syncthing: Don't clear sync volumes on startup
    This will allow bind volumes to persist across `blimp down`s. This decreases
    startup time for subsequent `blimp up`s by syncing only new files/changes.

--- sandbox/syncthing/main.go
+++ sandbox/syncthing/main.go
@@ -4,24 +4,12 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
-	"path/filepath"
 
-	log "github.com/sirupsen/logrus"
-
-	"github.com/kelda/blimp/pkg/errors"
 	"github.com/kelda/blimp/pkg/syncthing"
 )
 
 func main() {
 	folders := syncthing.ArgsToMap(os.Args[1:])
-
-	// Reset the bind volume so that we don't sync stale files back to the user.
-	// The volume should already be empty because of the volume finalizer that
-	// gets run when a namespace is deleted, but we check again here just in
-	// case.
-	if err := clearDir("/bind"); err != nil {
-		log.WithError(err).Fatal("Failed to clear path")
-	}
 
 	err := syncthing.MakeMarkers(folders)
 	if err != nil {
@@ -41,21 +29,4 @@ func main() {
 	if err := cmd.Run(); err != nil {
 		panic(err)
 	}
-}
-
-func clearDir(dir string) error {
-	paths, err := ioutil.ReadDir(dir)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return nil
-		}
-		return errors.WithContext("read dir", err)
-	}
-
-	for _, path := range paths {
-		if err := os.RemoveAll(filepath.Join(dir, path.Name())); err != nil {
-			return errors.WithContext("remove", err)
-		}
-	}
-	return nil
 }
