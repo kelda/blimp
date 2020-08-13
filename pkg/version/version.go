6cfeb7b19b2258ef18d2fe1f35d6d784a12624bf
Christopher Cooper <christopher@kelda.io>
2020-08-13 14:54:55 -0700

    version: Allow overriding blimp image repo at runtime
    This allows you to store the blimp system images in your own docker repo without
    rebuilding blimp to use the new image paths.

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,5 +1,7 @@
 package version
 
+import "os"
+
 var (
 	Version             = ""
 	NodeControllerImage = ""
@@ -8,3 +10,16 @@ var (
 	SyncthingImage      = ""
 	DNSImage            = ""
 )
+
+func init() {
+	repo, ok := os.LookupEnv("BLIMP_DOCKER_REPO")
+	if !ok || repo == "" {
+		return
+	}
+
+	NodeControllerImage = repo + "/blimp-node-controller:" + Version
+	InitImage = repo + "/blimp-init:" + Version
+	ReservationImage = repo + "/sandbox-reservation:" + Version
+	SyncthingImage = repo + "/sandbox-syncthing:" + Version
+	DNSImage = repo + "/blimp-dns:" + Version
+}
