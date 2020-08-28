bc7ebce5e13c20334a7d1c477fe88f5760bda3bf
Christopher Cooper <christopher@kelda.io>
2020-08-28 16:41:08 -0700

    cli: Make Docker daemon check more resilient
    Before, we would try to check if the correct unix socket existed. Now, we simply
    issue a Ping request to see if the server responds.
    
    Also, don't check for cached images if we don't have a docker daemon. This could
    cause a panic.

--- cli/util/util.go
+++ cli/util/util.go
@@ -1,10 +1,9 @@
 package util
 
 import (
+	"context"
 	"crypto/x509"
 	"io/ioutil"
-	"net/url"
-	"os"
 	"strings"
 	"time"
 
@@ -32,38 +31,56 @@ func Dial(addr, certPEM, serverNameOverride string) (*grpc.ClientConn, error) {
 		grpc.WithUnaryInterceptor(errors.UnaryClientInterceptor))
 }
 
+// GetDockerClient returns a working Docker client, or nil if we can't connect
+// to a Docker client. Notably, this function can return (nil, nil) if we can't
+// find a Docker client that accepts connetions.
 func GetDockerClient() (*client.Client, error) {
 	dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, errors.WithContext("create docker client", err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 5 * time.Second)
+	_, err = dockerClient.Ping(ctx)
+	if err == nil {
+		// We successfully connected to the Docker daemon, so we're pretty
+		// confident it's running and works.
+		return dockerClient, nil
+	}
+	if !client.IsErrConnectionFailed(err) {
+		return nil, errors.WithContext("docker ping failed", err)
+	}
+
+	// If connection failed, Docker is probably not available at the default
+	// address.
 
 	// If a custom host is set, don't try any funny business.
-	if err != nil || dockerClient.DaemonHost() != client.DefaultDockerHost {
-		return dockerClient, err
+	if dockerClient.DaemonHost() != client.DefaultDockerHost {
+		return nil, nil
 	}
 
-	socketUrl, err := url.Parse(dockerClient.DaemonHost())
-	if err != nil {
-		// We really do not expect this to ever happen.
-		panic(err)
-	}
-
-	if socketUrl.Scheme != "unix" {
-		return dockerClient, nil
-	}
-
-	// Check to see if this unix socket actually exists.
-	f, err := os.Open(socketUrl.Path)
-	if err == nil {
-		f.Close()
-		return dockerClient, nil
-	}
-
-	// It doesn't exist. If we're in WSL, see if we should use the TCP socket
-	// instead.
+	// If we're in WSL, see if we should use the TCP socket instead.
 	procVersion, err := ioutil.ReadFile("/proc/version")
 	if err != nil || !strings.Contains(string(procVersion), "Microsoft") {
-		return dockerClient, nil
+		// Not WSL, so we just give up.
+		return nil, nil
 	}
 
-	return client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation(),
+	dockerClient, err = client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation(),
 		client.WithHost("tcp://localhost:2375"))
+	if err != nil {
+		return nil, errors.WithContext("create WSL TCP docker client", err)
+	}
+
+	ctx, _ = context.WithTimeout(context.Background(), 5 * time.Second)
+	_, err = dockerClient.Ping(ctx)
+	if err == nil {
+		return dockerClient, nil
+	}
+	if !client.IsErrConnectionFailed(err) {
+		return nil, errors.WithContext("docker ping failed", err)
+	}
+
+	// Still unable to find a Docker daemon. Give up.
+	return nil, nil
 }
